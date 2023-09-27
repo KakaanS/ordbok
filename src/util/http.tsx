328f02301bd11335fc35cf7e56a65f08fad89548
Oscar Wendt <oscar.wendt91@gmail.com>
2023-09-27 15:03:49 +0200

    bugfixes

--- src/util/http.tsx
+++ src/util/http.tsx
@@ -6,5 +6,12 @@ const wordURL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 export async function fetchWordData(word: string) {
   const response = await fetch(`${wordURL}${word}`);
-  return response.json();
+  const data = await response.json();
+  console.log(data);
+
+  if (!response.ok) {
+    throw new Error(data.title || "Could not fetch word data");
+  }
+
+  return data;
 }
